254e428db51333c23977c27a76418058586ca811
whois <whois_1982@yahoo.co.in>
2015-02-11 07:20:53 -0800

    list.h list.c added
    list.h - linked list header file
    list.c - implementation of linked list

---
+++ list.h
@@ -0,0 +1,35 @@
+#ifndef LIST.H
+#define LIST.H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct ListElement_ {
+        void *data;
+        ListElement_*next;
+} ListElement;
+
+typedef struct List {
+        int size;
+        int (*match) (const void *key1, const void *key2);
+        void (*destroy) (void *data);
+
+        List *head;
+        List *tail;
+}List;
+
+void list_init(List *list, void (*destroy) (void *data));
+void list_destroy(List *list);
+int list_insert_next(List *list, ListElement *element, const void *data);
+int list_remove_next(List *list, ListElement *element, const void *data);
+
+#define list_size(list) ((list)->size)
+#define list_head(list) ((list)->head)
+#define list_tail(list) ((list)->tail)
+#define list_is_head(list,element) ((element) == (list)->head ? 1:0)
+#define list_is_tail(list,element) ((element)->next == NULL ? 1:0)
+#define list_data(element) ((element)->data)
+#define list_next(element) ((element->next)
+
+#endif
+
