9e9d4529a8f237a9178bcf35f134894fe4927c3d
Bhargava <bhargavanaryana@gmail.com>
2016-05-29 22:08:04 -0700

    module: dir fot LKM's;hello: hello world LKM

---
+++ module/hello/hello.c
@@ -0,0 +1,36 @@
+/*hello.c
+ *
+ *Simple hello world LKM (Loadable Kernel Module)
+ *
+ *
+ * Usage: 
+ *      make
+ *      insmod hello.ko
+ *      dmesg | tail
+ *      rmmod hello
+ *      dmesg | tail
+ */ 
+
+
+#define __KERNEL__
+#define MODULE
+
+//standard headers for LKM's
+//#include <linux/modversions.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+#include <linux/tty.h> // console_print() interface
+
+//init LKM
+int init_module()
+{
+        printk("Hello World!!\n");
+
+        return 0;
+}
+
+void cleanup_module()
+{
+        printk("This is the short life of a LKM\n");
+}
